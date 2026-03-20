41b01329db84beb8ce92166cf47b4cd9b910291a
derick871 <wanyamaderrick4@gmail.com>
2026-03-20 15:43:41 +0300

    add read me and  html files linked with tailwind

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+let firstName = "";
+let lastName = `Watako`;
+console.log(`welcome, ${firstName}  ${lastName}`);
+var a = 34;
+var b = 56;
+console.log(a+b);
+ a = 258; // integers
+ b = "34.2"; // float (has decimal point)
+console.log(a+b);
+let xyz = "abcd"; //string
+console.log(xyz);
+console.log(typeof(b)); //checking data types
+console.log(a + parseInt(b)); //change data types
+console.log(a + parseFloat(b)); //change data types
+console.log(10 / 3);
+console.log(10 % 3); //modulous
+let _dogs = ["rex", "simba", "papi"] //array
