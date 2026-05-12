5cfae9cf46d80d65d45965efda8753b7c489fb4e
derick871 <wanyamaderrick4@gmail.com>
2026-05-12 16:49:26 +0300

    modify the pages

--- script.js
+++ script.js
@@ -16,3 +16,71 @@ console.log(a + parseFloat(b)); //change data types
 console.log(10 / 3);
 console.log(10 % 3); //modulous
 let _dogs = ["rex", "simba", "papi"] //array
+alert ('hello'); 
+// boolean define true or false
+// double == store 
+// node script.js it run the code
+//string concatenation combine strings  console.log('hello' + derrick)
+// to separate strings add space console.log('hello ' + derick)
+// let programming =("java","c","c#")
+//result =programming/log(',')
+//console.log(result)
+let count;
+//for (let count=0;count <10; count ++) {
+console.log(count)
+}
+// copmparison operators
+a==b
+//stict operator
+a===b
+// <= lee or equal to
+//let age =20
+if (age>=18){
+    console.log(you can vote)
+}else if(age >=16){
+    console.log(you are not eligible to vote)
+}else {
+    console.log("you are a toddler!Go get breast milk")
+}
+console.log("hello");
+\n //new line
+`hello`// interpolation
+'items ({1+1})' :$ ${(2095+799)/100}
+ 'some
+ text'//multi line strings
+
+ 'some\ntext'
+
+  /'n/   // escape characters
+  >=0 $$   //if number range from 0
+  >=1/3  // range from 1 to 3
+  
+  console.log(true // false); //OR operator
+   // Scope -scope limits where a variable exists
+   // else if(randomNumber >= 1/3 && randomNumber <2/3)
+   // else if (randomNumber >=2/3 && randomNumber <1)
+   //const computermove;we replace the const with let.
+   any variable inside {...} only exists as {...}
+   // falsey value: 0 '' null nan undefine
+
+  // shortcuts for if statement
+   -ternary operator ?:
+   //let vote =(age>=18)? "you can vote" : "Go to school and read"
+   //console.log(vote)
+   -guard operator &&
+   -default operator ||
+
+   functions
+   console.log('hello world');
+   let firstName="derick"
+   let lastName=("wanyama");
+
+   console.log(welcome,${firstName} + ${lastName});
+
+
+
+
+
+
+
+
